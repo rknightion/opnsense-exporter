02748e57afed895ff71bdaa951b7e6c12f76ad74
Rob Knight <12484127+rknightion@users.noreply.github.com>
2026-03-01 17:19:38 +0000

    feat: enhance unbound DNS collector with comprehensive metrics

--- internal/collector/unbound_dns.go
+++ internal/collector/unbound_dns.go
@@ -11,6 +11,42 @@ type unboundDNSCollector struct {
 	log    *slog.Logger
 	uptime *prometheus.Desc
 
+	// Counter descriptors (no extra labels)
+	queriesTotal         *prometheus.Desc
+	cacheHitsTotal       *prometheus.Desc
+	cacheMissTotal       *prometheus.Desc
+	prefetchTotal        *prometheus.Desc
+	expiredTotal         *prometheus.Desc
+	recursiveReplies     *prometheus.Desc
+	queriesTimedOutTotal *prometheus.Desc
+	queriesIPRatelimited *prometheus.Desc
+	answersSecureTotal   *prometheus.Desc
+	answersBogusTotal    *prometheus.Desc
+	rrsetBogusTotal      *prometheus.Desc
+
+	// Counter descriptors (with labels)
+	queriesByType   *prometheus.Desc
+	queriesByProto  *prometheus.Desc
+	answersByRcode  *prometheus.Desc
+	unwantedTotal   *prometheus.Desc
+	queryFlagsTotal *prometheus.Desc
+	ednsTotal       *prometheus.Desc
+
+	// Gauge descriptors (no extra labels)
+	requestListAvg      *prometheus.Desc
+	requestListMax      *prometheus.Desc
+	recursionTimeAvg    *prometheus.Desc
+	recursionTimeMedian *prometheus.Desc
+
+	// Gauge descriptors (with labels)
+	cacheCount         *prometheus.Desc
+	memoryBytes        *prometheus.Desc
+	requestListCurrent *prometheus.Desc
+
+	// Counter descriptors (no extra labels, request list)
+	requestListOverwritten *prometheus.Desc
+	requestListExceeded    *prometheus.Desc
+
 	subsystem string
 	instance  string
 }
@@ -34,10 +70,150 @@ func (c *unboundDNSCollector) Register(namespace, instanceLabel string, log *slo
 		"Uptime of the unbound DNS service in seconds",
 		nil,
 	)
+
+	// Counters without extra labels
+	c.queriesTotal = buildPrometheusDesc(c.subsystem, "queries_total",
+		"Total number of queries received",
+		nil,
+	)
+	c.cacheHitsTotal = buildPrometheusDesc(c.subsystem, "cache_hits_total",
+		"Total number of cache hits",
+		nil,
+	)
+	c.cacheMissTotal = buildPrometheusDesc(c.subsystem, "cache_miss_total",
+		"Total number of cache misses",
+		nil,
+	)
+	c.prefetchTotal = buildPrometheusDesc(c.subsystem, "prefetch_total",
+		"Total number of cache prefetches",
+		nil,
+	)
+	c.expiredTotal = buildPrometheusDesc(c.subsystem, "expired_total",
+		"Total number of expired entries served",
+		nil,
+	)
+	c.recursiveReplies = buildPrometheusDesc(c.subsystem, "recursive_replies_total",
+		"Total number of recursive replies sent",
+		nil,
+	)
+	c.queriesTimedOutTotal = buildPrometheusDesc(c.subsystem, "queries_timed_out_total",
+		"Total number of queries that timed out",
+		nil,
+	)
+	c.queriesIPRatelimited = buildPrometheusDesc(c.subsystem, "queries_ip_ratelimited_total",
+		"Total number of queries that were IP rate limited",
+		nil,
+	)
+	c.answersSecureTotal = buildPrometheusDesc(c.subsystem, "answers_secure_total",
+		"Total number of DNSSEC secure answers",
+		nil,
+	)
+	c.answersBogusTotal = buildPrometheusDesc(c.subsystem, "answers_bogus_total",
+		"Total number of DNSSEC bogus answers",
+		nil,
+	)
+	c.rrsetBogusTotal = buildPrometheusDesc(c.subsystem, "rrset_bogus_total",
+		"Total number of DNSSEC bogus rrsets",
+		nil,
+	)
+
+	// Counters with labels
+	c.queriesByType = buildPrometheusDesc(c.subsystem, "queries_by_type_total",
+		"Total queries by DNS record type",
+		[]string{"type"},
+	)
+	c.queriesByProto = buildPrometheusDesc(c.subsystem, "queries_by_protocol_total",
+		"Total queries by protocol",
+		[]string{"protocol"},
+	)
+	c.answersByRcode = buildPrometheusDesc(c.subsystem, "answers_by_rcode_total",
+		"Total answers by response code",
+		[]string{"rcode"},
+	)
+	c.unwantedTotal = buildPrometheusDesc(c.subsystem, "unwanted_total",
+		"Total number of unwanted queries or replies",
+		[]string{"type"},
+	)
+	c.queryFlagsTotal = buildPrometheusDesc(c.subsystem, "query_flags_total",
+		"Total queries by DNS flag",
+		[]string{"flag"},
+	)
+	c.ednsTotal = buildPrometheusDesc(c.subsystem, "edns_total",
+		"Total EDNS queries by type",
+		[]string{"type"},
+	)
+
+	// Gauges without extra labels
+	c.requestListAvg = buildPrometheusDesc(c.subsystem, "request_list_avg",
+		"Average number of requests in the internal request list",
+		nil,
+	)
+	c.requestListMax = buildPrometheusDesc(c.subsystem, "request_list_max",
+		"Maximum number of requests in the internal request list",
+		nil,
+	)
+	c.recursionTimeAvg = buildPrometheusDesc(c.subsystem, "recursion_time_avg_seconds",
+		"Average recursion time in seconds",
+		nil,
+	)
+	c.recursionTimeMedian = buildPrometheusDesc(c.subsystem, "recursion_time_median_seconds",
+		"Median recursion time in seconds",
+		nil,
+	)
+
+	// Gauges with labels
+	c.cacheCount = buildPrometheusDesc(c.subsystem, "cache_count",
+		"Number of entries in cache by cache type",
+		[]string{"cache"},
+	)
+	c.memoryBytes = buildPrometheusDesc(c.subsystem, "memory_bytes",
+		"Memory usage in bytes by component",
+		[]string{"component"},
+	)
+	c.requestListCurrent = buildPrometheusDesc(c.subsystem, "request_list_current",
+		"Current number of requests in the internal request list by scope",
+		[]string{"scope"},
+	)
+
+	// Counters without extra labels (request list)
+	c.requestListOverwritten = buildPrometheusDesc(c.subsystem, "request_list_overwritten_total",
+		"Total number of request list entries overwritten by newer entries",
+		nil,
+	)
+	c.requestListExceeded = buildPrometheusDesc(c.subsystem, "request_list_exceeded_total",
+		"Total number of request list entries that exceeded the maximum",
+		nil,
+	)
 }
 
 func (c *unboundDNSCollector) Describe(ch chan<- *prometheus.Desc) {
 	ch <- c.uptime
+	ch <- c.queriesTotal
+	ch <- c.cacheHitsTotal
+	ch <- c.cacheMissTotal
+	ch <- c.prefetchTotal
+	ch <- c.expiredTotal
+	ch <- c.recursiveReplies
+	ch <- c.queriesTimedOutTotal
+	ch <- c.queriesIPRatelimited
+	ch <- c.answersSecureTotal
+	ch <- c.answersBogusTotal
+	ch <- c.rrsetBogusTotal
+	ch <- c.queriesByType
+	ch <- c.queriesByProto
+	ch <- c.answersByRcode
+	ch <- c.unwantedTotal
+	ch <- c.queryFlagsTotal
+	ch <- c.ednsTotal
+	ch <- c.requestListAvg
+	ch <- c.requestListMax
+	ch <- c.recursionTimeAvg
+	ch <- c.recursionTimeMedian
+	ch <- c.cacheCount
+	ch <- c.memoryBytes
+	ch <- c.requestListCurrent
+	ch <- c.requestListOverwritten
+	ch <- c.requestListExceeded
 }
 
 func (c *unboundDNSCollector) Update(client *opnsense.Client, ch chan<- prometheus.Metric) *opnsense.APICallError {
@@ -45,11 +221,187 @@ func (c *unboundDNSCollector) Update(client *opnsense.Client, ch chan<- promethe
 	if err != nil {
 		return err
 	}
+
+	// Uptime gauge
 	ch <- prometheus.MustNewConstMetric(
 		c.uptime,
 		prometheus.GaugeValue,
-		float64(data.UptimeSeconds),
+		data.UptimeSeconds,
 		c.instance,
+	)
+
+	// Counters without extra labels
+	ch <- prometheus.MustNewConstMetric(
+		c.queriesTotal, prometheus.CounterValue,
+		float64(data.QueriesTotal), c.instance,
+	)
+	ch <- prometheus.MustNewConstMetric(
+		c.cacheHitsTotal, prometheus.CounterValue,
+		float64(data.CacheHits), c.instance,
+	)
+	ch <- prometheus.MustNewConstMetric(
+		c.cacheMissTotal, prometheus.CounterValue,
+		float64(data.CacheMiss), c.instance,
+	)
+	ch <- prometheus.MustNewConstMetric(
+		c.prefetchTotal, prometheus.CounterValue,
+		float64(data.Prefetch), c.instance,
+	)
+	ch <- prometheus.MustNewConstMetric(
+		c.expiredTotal, prometheus.CounterValue,
+		float64(data.ExpiredTotal), c.instance,
+	)
+	ch <- prometheus.MustNewConstMetric(
+		c.recursiveReplies, prometheus.CounterValue,
+		float64(data.RecursiveReplies), c.instance,
+	)
+	ch <- prometheus.MustNewConstMetric(
+		c.queriesTimedOutTotal, prometheus.CounterValue,
+		float64(data.QueriesTimedOut), c.instance,
+	)
+	ch <- prometheus.MustNewConstMetric(
+		c.queriesIPRatelimited, prometheus.CounterValue,
+		float64(data.QueriesIPRateLimited), c.instance,
+	)
+	ch <- prometheus.MustNewConstMetric(
+		c.answersSecureTotal, prometheus.CounterValue,
+		float64(data.AnswerSecureTotal), c.instance,
+	)
+	ch <- prometheus.MustNewConstMetric(
+		c.answersBogusTotal, prometheus.CounterValue,
+		float64(data.AnswerBogusTotal), c.instance,
+	)
+	ch <- prometheus.MustNewConstMetric(
+		c.rrsetBogusTotal, prometheus.CounterValue,
+		float64(data.RrsetBogusTotal), c.instance,
+	)
+
+	// Queries by type
+	for qtype, count := range data.QueryTypesByType {
+		ch <- prometheus.MustNewConstMetric(
+			c.queriesByType, prometheus.CounterValue,
+			float64(count), qtype, c.instance,
+		)
+	}
+
+	// Queries by protocol
+	protocols := map[string]int{
+		"tcp":    data.QueryTCP,
+		"tcpout": data.QueryTCPOut,
+		"udpout": data.QueryUDPOut,
+		"tls":    data.QueryTLS,
+		"ipv6":   data.QueryIPv6,
+		"https":  data.QueryHTTPS,
+	}
+	for proto, count := range protocols {
+		ch <- prometheus.MustNewConstMetric(
+			c.queriesByProto, prometheus.CounterValue,
+			float64(count), proto, c.instance,
+		)
+	}
+
+	// Answers by rcode
+	for rcode, count := range data.AnswerRcodesByRcode {
+		ch <- prometheus.MustNewConstMetric(
+			c.answersByRcode, prometheus.CounterValue,
+			float64(count), rcode, c.instance,
+		)
+	}
+
+	// Unwanted
+	ch <- prometheus.MustNewConstMetric(
+		c.unwantedTotal, prometheus.CounterValue,
+		float64(data.UnwantedQueries), "queries", c.instance,
+	)
+	ch <- prometheus.MustNewConstMetric(
+		c.unwantedTotal, prometheus.CounterValue,
+		float64(data.UnwantedReplies), "replies", c.instance,
+	)
+
+	// Query flags
+	for flag, count := range data.FlagsByFlag {
+		ch <- prometheus.MustNewConstMetric(
+			c.queryFlagsTotal, prometheus.CounterValue,
+			float64(count), flag, c.instance,
+		)
+	}
+
+	// EDNS
+	ch <- prometheus.MustNewConstMetric(
+		c.ednsTotal, prometheus.CounterValue,
+		float64(data.EdnsPresent), "present", c.instance,
+	)
+	ch <- prometheus.MustNewConstMetric(
+		c.ednsTotal, prometheus.CounterValue,
+		float64(data.EdnsDO), "DO", c.instance,
+	)
+
+	// Gauges without extra labels
+	ch <- prometheus.MustNewConstMetric(
+		c.requestListAvg, prometheus.GaugeValue,
+		data.RequestListAvg, c.instance,
+	)
+	ch <- prometheus.MustNewConstMetric(
+		c.requestListMax, prometheus.GaugeValue,
+		float64(data.RequestListMax), c.instance,
+	)
+	ch <- prometheus.MustNewConstMetric(
+		c.recursionTimeAvg, prometheus.GaugeValue,
+		data.RecursionTimeAvg, c.instance,
+	)
+	ch <- prometheus.MustNewConstMetric(
+		c.recursionTimeMedian, prometheus.GaugeValue,
+		data.RecursionTimeMedian, c.instance,
+	)
+
+	// Cache counts
+	caches := map[string]int{
+		"rrset":   data.CacheRrsetCount,
+		"message": data.CacheMessageCount,
+		"infra":   data.CacheInfraCount,
+		"key":     data.CacheKeyCount,
+	}
+	for cache, count := range caches {
+		ch <- prometheus.MustNewConstMetric(
+			c.cacheCount, prometheus.GaugeValue,
+			float64(count), cache, c.instance,
+		)
+	}
+
+	// Memory bytes
+	memComponents := map[string]int{
+		"rrset_cache":   data.MemCacheRrset,
+		"message_cache": data.MemCacheMessage,
+		"iterator":      data.MemModIterator,
+		"validator":     data.MemModValidator,
+		"respip":        data.MemModRespip,
+		"streamwait":    data.MemStreamwait,
+	}
+	for component, bytes := range memComponents {
+		ch <- prometheus.MustNewConstMetric(
+			c.memoryBytes, prometheus.GaugeValue,
+			float64(bytes), component, c.instance,
+		)
+	}
+
+	// Request list current
+	ch <- prometheus.MustNewConstMetric(
+		c.requestListCurrent, prometheus.GaugeValue,
+		float64(data.RequestListCurrentAll), "all", c.instance,
+	)
+	ch <- prometheus.MustNewConstMetric(
+		c.requestListCurrent, prometheus.GaugeValue,
+		float64(data.RequestListCurrentUser), "user", c.instance,
+	)
+
+	// Request list counters
+	ch <- prometheus.MustNewConstMetric(
+		c.requestListOverwritten, prometheus.CounterValue,
+		float64(data.RequestListOverwritten), c.instance,
+	)
+	ch <- prometheus.MustNewConstMetric(
+		c.requestListExceeded, prometheus.CounterValue,
+		float64(data.RequestListExceeded), c.instance,
 	)
 
 	return nil
--- opnsense/unbound_dns.go
+++ opnsense/unbound_dns.go
@@ -10,18 +10,21 @@ type unboundDNSStatusResponse struct {
 	Data   struct {
 		Total struct {
 			Num struct {
-				Queries              string `json:"queries"`
-				QueriesIPRatelimited string `json:"queries_ip_ratelimited"`
-				QueriesCookieValid   string `json:"queries_cookie_valid"`
-				QueriesCookieClient  string `json:"queries_cookie_client"`
-				QueriesCookieInvalid string `json:"queries_cookie_invalid"`
-				Cachehits            string `json:"cachehits"`
-				Cachemiss            string `json:"cachemiss"`
-				Prefetch             string `json:"prefetch"`
-				QueriesTimedOut      string `json:"queries_timed_out"`
-				Expired              string `json:"expired"`
-				Recursivereplies     string `json:"recursivereplies"`
-				Dnscrypt             struct {
+				Queries               string `json:"queries"`
+				QueriesIPRatelimited  string `json:"queries_ip_ratelimited"`
+				QueriesCookieValid    string `json:"queries_cookie_valid"`
+				QueriesCookieClient   string `json:"queries_cookie_client"`
+				QueriesCookieInvalid  string `json:"queries_cookie_invalid"`
+				Cachehits             string `json:"cachehits"`
+				Cachemiss             string `json:"cachemiss"`
+				Prefetch              string `json:"prefetch"`
+				QueriesTimedOut       string `json:"queries_timed_out"`
+				Expired               string `json:"expired"`
+				Recursivereplies      string `json:"recursivereplies"`
+				QueriesDiscardTimeout string `json:"queries_discard_timeout"`
+				QueriesWaitLimit      string `json:"queries_wait_limit"`
+				DNSErrorReports       string `json:"dns_error_reports"`
+				Dnscrypt              struct {
 					Crypted   string `json:"crypted"`
 					Cert      string `json:"cert"`
 					Cleartext string `json:"cleartext"`
@@ -78,15 +81,22 @@ type unboundDNSStatusResponse struct {
 		Num struct {
 			Query struct {
 				Type struct {
-					A     string `json:"A"`
-					Soa   string `json:"SOA"`
-					Ptr   string `json:"PTR"`
-					Mx    string `json:"MX"`
-					Txt   string `json:"TXT"`
-					Aaaa  string `json:"AAAA"`
-					Srv   string `json:"SRV"`
-					Svcb  string `json:"SVCB"`
-					HTTPS string `json:"HTTPS"`
+					A      string `json:"A"`
+					Soa    string `json:"SOA"`
+					Ptr    string `json:"PTR"`
+					Mx     string `json:"MX"`
+					Txt    string `json:"TXT"`
+					Aaaa   string `json:"AAAA"`
+					Srv    string `json:"SRV"`
+					Svcb   string `json:"SVCB"`
+					HTTPS  string `json:"HTTPS"`
+					NS     string `json:"NS"`
+					CNAME  string `json:"CNAME"`
+					NAPTR  string `json:"NAPTR"`
+					DNSKEY string `json:"DNSKEY"`
+					ANY    string `json:"ANY"`
+					LOC    string `json:"LOC"`
+					HINFO  string `json:"HINFO"`
 				} `json:"type"`
 				Class struct {
 					In string `json:"IN"`
@@ -190,23 +200,83 @@ type unboundDNSStatusResponse struct {
 }
 
 type UnboundDNSOverview struct {
-	AnswerRcodes      map[string]int
-	QueryTypes        map[string]int
-	Total             int
-	BlocklistSize     int
-	Passed            int
-	AnswerRcodesTotal int
-	AnnswerBogusTotal int
+	UptimeSeconds float64
+
+	// Query totals (from data.total.num)
+	QueriesTotal         int
+	CacheHits            int
+	CacheMiss            int
+	Prefetch             int
+	QueriesTimedOut      int
+	ExpiredTotal         int
+	RecursiveReplies     int
+	QueriesIPRateLimited int
+
+	// Query types (from data.num.query.type) - map label->count
+	QueryTypesByType map[string]int
+
+	// Query protocols
+	QueryTCP    int
+	QueryTCPOut int
+	QueryUDPOut int
+	QueryTLS    int
+	QueryIPv6   int
+	QueryHTTPS  int
+
+	// Answer rcodes - map label->count
+	AnswerRcodesByRcode map[string]int
+
+	// DNSSEC
 	AnswerSecureTotal int
-	UptimeSeconds     float64
+	AnswerBogusTotal  int
+	RrsetBogusTotal   int
+
+	// Cache entry counts
+	CacheRrsetCount   int
+	CacheMessageCount int
+	CacheInfraCount   int
+	CacheKeyCount     int
+
+	// Memory in bytes
+	MemCacheRrset   int
+	MemCacheMessage int
+	MemModIterator  int
+	MemModValidator int
+	MemModRespip    int
+	MemStreamwait   int
+
+	// Request list
+	RequestListAvg         float64
+	RequestListMax         int
+	RequestListOverwritten int
+	RequestListExceeded    int
+	RequestListCurrentAll  int
+	RequestListCurrentUser int
+
+	// Recursion time
+	RecursionTimeAvg    float64
+	RecursionTimeMedian float64
+
+	// TCP usage
+	TCPUsage float64
+
+	// Query flags - map label->count
+	FlagsByFlag map[string]int
+
+	// EDNS
+	EdnsPresent int
+	EdnsDO      int
+
+	// Unwanted
+	UnwantedQueries int
+	UnwantedReplies int
 }
 
 func (c *Client) FetchUnboundOverview() (UnboundDNSOverview, *APICallError) {
 	var (
-		response      unboundDNSStatusResponse
-		data          UnboundDNSOverview
-		err           error
-		errConvertion *APICallError
+		response unboundDNSStatusResponse
+		data     UnboundDNSOverview
+		err      error
 	)
 
 	url, ok := c.endpoints["unboundDNSStatus"]
@@ -221,9 +291,7 @@ func (c *Client) FetchUnboundOverview() (UnboundDNSOverview, *APICallError) {
 		return data, err
 	}
 
-	data.QueryTypes = make(map[string]int)
-	data.AnswerRcodes = make(map[string]int)
-
+	// Uptime
 	data.UptimeSeconds, err = strconv.ParseFloat(response.Data.Time.Up, 64)
 	if err != nil {
 		return data, &APICallError{
@@ -232,13 +300,107 @@ func (c *Client) FetchUnboundOverview() (UnboundDNSOverview, *APICallError) {
 			StatusCode: 0,
 		}
 	}
-	data.AnnswerBogusTotal, errConvertion = parseStringToInt(response.Data.Num.Answer.Bogus, url)
-	if errConvertion != nil {
-		return data, errConvertion
+
+	// Query totals
+	data.QueriesTotal = safeAtoi(response.Data.Total.Num.Queries)
+	data.CacheHits = safeAtoi(response.Data.Total.Num.Cachehits)
+	data.CacheMiss = safeAtoi(response.Data.Total.Num.Cachemiss)
+	data.Prefetch = safeAtoi(response.Data.Total.Num.Prefetch)
+	data.QueriesTimedOut = safeAtoi(response.Data.Total.Num.QueriesTimedOut)
+	data.ExpiredTotal = safeAtoi(response.Data.Total.Num.Expired)
+	data.RecursiveReplies = safeAtoi(response.Data.Total.Num.Recursivereplies)
+	data.QueriesIPRateLimited = safeAtoi(response.Data.Total.Num.QueriesIPRatelimited)
+
+	// Query types
+	data.QueryTypesByType = map[string]int{
+		"A":      safeAtoi(response.Data.Num.Query.Type.A),
+		"AAAA":   safeAtoi(response.Data.Num.Query.Type.Aaaa),
+		"SOA":    safeAtoi(response.Data.Num.Query.Type.Soa),
+		"PTR":    safeAtoi(response.Data.Num.Query.Type.Ptr),
+		"MX":     safeAtoi(response.Data.Num.Query.Type.Mx),
+		"TXT":    safeAtoi(response.Data.Num.Query.Type.Txt),
+		"SRV":    safeAtoi(response.Data.Num.Query.Type.Srv),
+		"SVCB":   safeAtoi(response.Data.Num.Query.Type.Svcb),
+		"HTTPS":  safeAtoi(response.Data.Num.Query.Type.HTTPS),
+		"NS":     safeAtoi(response.Data.Num.Query.Type.NS),
+		"CNAME":  safeAtoi(response.Data.Num.Query.Type.CNAME),
+		"NAPTR":  safeAtoi(response.Data.Num.Query.Type.NAPTR),
+		"DNSKEY": safeAtoi(response.Data.Num.Query.Type.DNSKEY),
+		"ANY":    safeAtoi(response.Data.Num.Query.Type.ANY),
 	}
-	data.AnswerSecureTotal, errConvertion = parseStringToInt(response.Data.Num.Answer.Secure, url)
-	if errConvertion != nil {
-		return data, errConvertion
+
+	// Query protocols
+	data.QueryTCP = safeAtoi(response.Data.Num.Query.TCP)
+	data.QueryTCPOut = safeAtoi(response.Data.Num.Query.Tcpout)
+	data.QueryUDPOut = safeAtoi(response.Data.Num.Query.Udpout)
+	data.QueryTLS = safeAtoi(response.Data.Num.Query.TLS.Value)
+	data.QueryIPv6 = safeAtoi(response.Data.Num.Query.Ipv6)
+	data.QueryHTTPS = safeAtoi(response.Data.Num.Query.HTTPS)
+
+	// Answer rcodes
+	data.AnswerRcodesByRcode = map[string]int{
+		"NOERROR":  safeAtoi(response.Data.Num.Answer.Rcode.Noerror),
+		"FORMERR":  safeAtoi(response.Data.Num.Answer.Rcode.Formerr),
+		"SERVFAIL": safeAtoi(response.Data.Num.Answer.Rcode.Servfail),
+		"NXDOMAIN": safeAtoi(response.Data.Num.Answer.Rcode.Nxdomain),
+		"NOTIMPL":  safeAtoi(response.Data.Num.Answer.Rcode.Notimpl),
+		"REFUSED":  safeAtoi(response.Data.Num.Answer.Rcode.Refused),
+		"nodata":   safeAtoi(response.Data.Num.Answer.Rcode.Nodata),
 	}
+
+	// DNSSEC
+	data.AnswerSecureTotal = safeAtoi(response.Data.Num.Answer.Secure)
+	data.AnswerBogusTotal = safeAtoi(response.Data.Num.Answer.Bogus)
+	data.RrsetBogusTotal = safeAtoi(response.Data.Num.Rrset.Bogus)
+
+	// Cache entry counts
+	data.CacheRrsetCount = safeAtoi(response.Data.Rrset.Cache.Count)
+	data.CacheMessageCount = safeAtoi(response.Data.Msg.Cache.Count)
+	data.CacheInfraCount = safeAtoi(response.Data.Infra.Cache.Count)
+	data.CacheKeyCount = safeAtoi(response.Data.Key.Cache.Count)
+
+	// Memory in bytes
+	data.MemCacheRrset = safeAtoi(response.Data.Mem.Cache.Rrset)
+	data.MemCacheMessage = safeAtoi(response.Data.Mem.Cache.Message)
+	data.MemModIterator = safeAtoi(response.Data.Mem.Mod.Iterator)
+	data.MemModValidator = safeAtoi(response.Data.Mem.Mod.Validator)
+	data.MemModRespip = safeAtoi(response.Data.Mem.Mod.Respip)
+	data.MemStreamwait = safeAtoi(response.Data.Mem.Streamwait)
+
+	// Request list
+	data.RequestListAvg = safeParseFloat(response.Data.Total.Requestlist.Avg)
+	data.RequestListMax = safeAtoi(response.Data.Total.Requestlist.Max)
+	data.RequestListOverwritten = safeAtoi(response.Data.Total.Requestlist.Overwritten)
+	data.RequestListExceeded = safeAtoi(response.Data.Total.Requestlist.Exceeded)
+	data.RequestListCurrentAll = safeAtoi(response.Data.Total.Requestlist.Current.All)
+	data.RequestListCurrentUser = safeAtoi(response.Data.Total.Requestlist.Current.User)
+
+	// Recursion time
+	data.RecursionTimeAvg = safeParseFloat(response.Data.Total.Recursion.Time.Avg)
+	data.RecursionTimeMedian = safeParseFloat(response.Data.Total.Recursion.Time.Median)
+
+	// TCP usage
+	data.TCPUsage = safeParseFloat(response.Data.Total.Tcpusage)
+
+	// Query flags
+	data.FlagsByFlag = map[string]int{
+		"QR": safeAtoi(response.Data.Num.Query.Flags.Qr),
+		"AA": safeAtoi(response.Data.Num.Query.Flags.Aa),
+		"TC": safeAtoi(response.Data.Num.Query.Flags.Tc),
+		"RD": safeAtoi(response.Data.Num.Query.Flags.Rd),
+		"RA": safeAtoi(response.Data.Num.Query.Flags.Ra),
+		"Z":  safeAtoi(response.Data.Num.Query.Flags.Z),
+		"AD": safeAtoi(response.Data.Num.Query.Flags.Ad),
+		"CD": safeAtoi(response.Data.Num.Query.Flags.Cd),
+	}
+
+	// EDNS
+	data.EdnsPresent = safeAtoi(response.Data.Num.Query.Edns.Present)
+	data.EdnsDO = safeAtoi(response.Data.Num.Query.Edns.Do)
+
+	// Unwanted
+	data.UnwantedQueries = safeAtoi(response.Data.Unwanted.Queries)
+	data.UnwantedReplies = safeAtoi(response.Data.Unwanted.Replies)
+
 	return data, nil
 }
