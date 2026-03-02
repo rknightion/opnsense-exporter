dfd501f83d19434b87033165beed75096b5811a7
Rob Knight <12484127+rknightion@users.noreply.github.com>
2026-03-02 15:37:35 +0000

    feat: add CLI flags for new collectors

--- internal/options/collectors.go
+++ internal/options/collectors.go
@@ -35,6 +35,14 @@ var (
 		"exporter.disable-firmware",
 		"Disable the scraping of the firmware metrics",
 	).Envar("OPNSENSE_EXPORTER_DISABLE_FIRMWARE").Default("false").Bool()
+	systemCollectorDisabled = kingpin.Flag(
+		"exporter.disable-system",
+		"Disable the scraping of system resource metrics (memory, uptime, disk, swap)",
+	).Envar("OPNSENSE_EXPORTER_DISABLE_SYSTEM").Default("false").Bool()
+	temperatureCollectorDisabled = kingpin.Flag(
+		"exporter.disable-temperature",
+		"Disable the scraping of temperature metrics",
+	).Envar("OPNSENSE_EXPORTER_DISABLE_TEMPERATURE").Default("false").Bool()
 	dnsmasqCollectorDisabled = kingpin.Flag(
 		"exporter.disable-dnsmasq",
 		"Disable the scraping of Dnsmasq DHCP leases",
@@ -43,34 +51,50 @@ var (
 		"exporter.enable-dnsmasq-details",
 		"Enable per-lease detail metrics for Dnsmasq DHCP (high cardinality on large networks)",
 	).Envar("OPNSENSE_EXPORTER_ENABLE_DNSMASQ_DETAILS").Default("false").Bool()
+	firewallRulesCollectorDisabled = kingpin.Flag(
+		"exporter.disable-firewall-rules",
+		"Disable the scraping of per-rule firewall statistics",
+	).Envar("OPNSENSE_EXPORTER_DISABLE_FIREWALL_RULES").Default("false").Bool()
+	firewallRulesDetailsEnabled = kingpin.Flag(
+		"exporter.enable-firewall-rules-details",
+		"Enable per-rule detail metrics for firewall rules (high cardinality on large rulesets)",
+	).Envar("OPNSENSE_EXPORTER_ENABLE_FIREWALL_RULES_DETAILS").Default("false").Bool()
 )
 
 // CollectorsDisableSwitch hold the enabled/disabled state of the collectors
 type CollectorsDisableSwitch struct {
-	ARP            bool
-	Cron           bool
-	Wireguard      bool
-	IPsec          bool
-	Unbound        bool
-	OpenVPN        bool
-	Firewall       bool
-	Firmware       bool
-	Dnsmasq        bool
-	DnsmasqDetails bool
+	ARP                  bool
+	Cron                 bool
+	Wireguard            bool
+	IPsec                bool
+	Unbound              bool
+	OpenVPN              bool
+	Firewall             bool
+	Firmware             bool
+	Dnsmasq              bool
+	DnsmasqDetails       bool
+	FirewallRules        bool
+	FirewallRulesDetails bool
+	System               bool
+	Temperature          bool
 }
 
 // CollectorsSwitches returns configured instances of CollectorsDisableSwitch
 func CollectorsSwitches() CollectorsDisableSwitch {
 	return CollectorsDisableSwitch{
-		ARP:            !*arpTableCollectorDisabled,
-		Cron:           !*cronTableCollectorDisabled,
-		Wireguard:      !*wireguardCollectorDisabled,
-		IPsec:          !*ipsecCollectorDisabled,
-		Unbound:        !*unboundCollectorDisabled,
-		OpenVPN:        !*openVPNCollectorDisabled,
-		Firewall:       !*firewallCollectorDisabled,
-		Firmware:       !*firmwareCollectorDisabled,
-		Dnsmasq:        !*dnsmasqCollectorDisabled,
-		DnsmasqDetails: *dnsmasqDetailsEnabled,
+		ARP:                  !*arpTableCollectorDisabled,
+		Cron:                 !*cronTableCollectorDisabled,
+		Wireguard:            !*wireguardCollectorDisabled,
+		IPsec:                !*ipsecCollectorDisabled,
+		Unbound:              !*unboundCollectorDisabled,
+		OpenVPN:              !*openVPNCollectorDisabled,
+		Firewall:             !*firewallCollectorDisabled,
+		Firmware:             !*firmwareCollectorDisabled,
+		Dnsmasq:              !*dnsmasqCollectorDisabled,
+		DnsmasqDetails:       *dnsmasqDetailsEnabled,
+		FirewallRules:        !*firewallRulesCollectorDisabled,
+		FirewallRulesDetails: *firewallRulesDetailsEnabled,
+		System:               !*systemCollectorDisabled,
+		Temperature:          !*temperatureCollectorDisabled,
 	}
 }
